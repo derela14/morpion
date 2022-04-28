ad4b3c029b47eebf83e14fb37c22f9b301bf5aa4
derela14 <lau13perdereau@gmail.com>
2022-04-28 23:39:41 +0200

    second commit

---
+++ lib/board.rb
@@ -0,0 +1,65 @@
+require 'bundler'
+Bundler.require
+
+
+class Board
+    #TO DO : la classe a 1 attr_accessor : un array/hash qui contient les BoardCases.
+    attr_accessor :case_list
+    #Optionnellement on peut aussi lui rajouter un autre sous le nom @count_turn pour compter le nombre de coups joué
+  
+  
+    def initialize
+      #TO DO :
+      @case_list = [0,0,0,0,0,0,0,0,0]
+      #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
+      #Ces instances sont rangées dans un array/hash qui est l'attr_accessor de la classe
+    end
+  
+    #renvoie 0 si coup invalide
+    #renvoie 1 si ok
+    def play_turn(joueur,caseNbr)
+        #joueur vaut 1 ou 2
+        #case est un entier entre 0 et 8
+        #tester si la case est vide:
+        if @case_list[caseNbr]!=0
+            return 0
+        else
+            @case_list[caseNbr]=joueur
+            return 1
+        end
+      #TO DO : une méthode qui :
+      #1) demande au bon joueur ce qu'il souhaite faire
+      #2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
+    end
+  
+    #retourne 1 ou 2 si le joueur correspondant a gagné
+    #retourne 3 si match nul
+    #retourne 0 sinon
+    def test_Status
+        #test victory
+        for k in 0..2 do
+            if @case_list[3*k] != 0 && @case_list[3*k] == @case_list[3*k+1] && @case_list[3*k+1] == @case_list[3*k+2]
+                return @case_list[3*k]
+            end
+        end
+        for k in 0..2 do
+            if @case_list[k] != 0 && @case_list[k] == @case_list[k+3] && @case_list[k+3] == @case_list[k+6]
+                return @case_list[3*k]
+            end
+        end
+        if @case_list[0] != 0 && @case_list[4] == @case_list[0] && @case_list[4] == @case_list[8]
+            return @case_list[3*k]
+        end
+        if @case_list[2] != 0 && @case_list[2] == @case_list[4] && @case_list[4] == @case_list[6]
+            return @case_list[3*k]
+        end
+        if @case_list.include?(0)
+            return 0
+        else
+            return 3
+        end
+      #TO DO : une méthode qui vérifie le plateau et indique s'il y a un vainqueur ou match nul
+    end
+end
+
+
---
+++ lib/game.rb
@@ -0,0 +1,88 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'board.rb'
+
+#status: 0=on going, 1=victoire du joueur 1, 2=victoire du joueur 2, 3=match nul
+class Game
+    #TO DO : la classe a plusieurs attr_accessor: le current_player (égal à un objet Player), le status (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
+    attr_accessor :joueur1,:joueur2,:board,:joueurCourant,:status,:premierJoueur
+    def initialize(joueur1,joueur2)
+        @joueur1=joueur1
+        @joueur2=joueur2
+        @board=Board.new()
+        @joueurCourant = 1
+        @status = board.test_Status()
+        @premierJoueur = 1
+      #TO DO : créé 2 joueurs, créé un board, met le status à "on going", défini un current_player
+    end
+  
+    # return 0 si non fini
+    # return 1 si terminé
+    # redondant avec l'attribut status, mais ça coute rien
+    def turn
+        correct_case=0
+        while !correct_case
+            puts "c'est au tour de #{@joueur1}, entrez une _case entre 0 et 8:"
+            caseNbr = gets.chomp.to_i
+            if caseNbr >=0 && caseNbr <9
+                correct_case = 1
+            end
+            if correct_case
+                result = @board.play_turn(@joueurCourant,caseNbr)
+                if !result
+                    puts "Case déjà prise"
+                    correct_case = 0
+                end
+            end
+        end
+        @status = board.test_Status()
+        if @status == 0
+            if @joueurCourant == 1
+                @joueurCourant = 2
+            else
+                @joueurCourant = 1
+            end
+            return 0
+        else
+            return 1
+        end 
+      #TO DO : méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
+    end
+  
+    def new_round
+        # TO DO : relance une partie en initialisant un nouveau board mais en gardant les mêmes joueurs.
+        @board = Board.new()
+        if @premierJoueur == 1
+            @premierJoueur = 2
+        else
+            @premierJoueur = 1
+        end
+        @joueurCourant = @premierJoueur
+    end
+  
+    def game_end
+        if @status == 1
+            puts "#{@joueur1} a gagné"
+            return 1
+        end
+        if @status == 2
+            puts "#{@joueur2} a gagné"
+            return 2
+        end
+        if @status == 3
+            puts "match nul"
+            return 3
+        end
+        return 0
+      # TO DO : permet l'affichage de fin de partie quand un vainqueur est détecté ou si il y a match nul
+    end    
+  
+    def displayBoard
+        puts "#{@board.case_list[0]}  #{@board.case_list[1]}  #{@board.case_list[2]}\n"
+        puts "#{@board.case_list[3]}  #{@board.case_list[4]}  #{@board.case_list[5]}\n"
+        puts "#{@board.case_list[6]}  #{@board.case_list[7]}  #{@board.case_list[8]}\n" 
+    end
+end
+
+binding.pry
